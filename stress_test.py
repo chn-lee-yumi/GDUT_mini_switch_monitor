abffe685e67cf5bb123bb9ff828064153797224e
chn.lee.yumi <chn.lee.yumi@gmail.com>
2019-06-03 13:30:49 +0800

    更新压力测试

--- stress_test.py
+++ stress_test.py
@@ -3,14 +3,14 @@ import requests
 import threading
 import time
 
-for number in range(3115000000, 3119000000, 100):
+for number in range(3115000000, 3119000000, 200):
     t0 = time.time()
     ts = []
     for x in range(0, 200):
         ts.append(
-            threading.Thread(target=requests.get,
-                             args=("https://gdutnic.com/api/warning?number=%d" % (number + x),)))
+            threading.Thread(target=requests.get, args=(
+                "https://mini-monitor.gdutnic.com/api/query?number=%d" % (number + x),)))  # 辣鸡学校，不给域名
     for t in ts:
         t.start()
-    ts[50].join()
+    ts[100].join()
     print(number, time.time() - t0)
